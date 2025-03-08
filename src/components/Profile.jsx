58925445a14cd26dcfe3962b0305142a5065e741
nschwags <nschwags@gmail.com>
2025-03-08 17:41:44 -0600

    fixed path

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-scroll";
 import pfp from "../assets/Nathanpfp.jpg";
 import "./profile.css";
-import "../app.css";
+import "../App.css";
 
 function Profile() {
   const changeColor = (color) => {
